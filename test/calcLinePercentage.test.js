89f33dca9f0816e17ac2d60517e496b622ee0d7e
okFox <smayfield@gmail.com>
2019-10-16 17:34:03 -0700

    now I think it will pass

--- test/calcLinePercentage.test.js
+++ test/calcLinePercentage.test.js
@@ -7,7 +7,7 @@ const test = QUnit.test;
 test('returns percentage', function(assert) {
     //Arrange
     // Set up your parameters and expectations
-    const percent = 50;
+    const percent = '50%';
     //Act 
     // Call the function you're testing and set the result to a const
     let answer = calcLinePercentage(10, 5); 
